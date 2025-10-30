73a9037a4da7be892369d2f75500194981f854e7
emory.merryman@gmail.com <emory.merryman@gmail.com>
2025-10-30 19:58:42 -0400

    bin

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
                         let
                             implementation =
                                 {
-                                    inputs ,
+                                    inputs ? [ ] ,
                                     name ,
                                     text
                                 } :
@@ -52,7 +52,7 @@
                                     check =
                                         {
                                             expected ,
-                                            inputs ,
+                                            inputs ? [ ] ,
                                             mkDerivation ,
                                             name ,
                                             text
