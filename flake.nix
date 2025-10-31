4a02079db4d057c0e583dd40d217eedfe47662df
emory.merryman@gmail.com <emory.merryman@gmail.com>
2025-10-30 20:09:03 -0400

    bin

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
                                     check =
                                         {
                                             expected ,
+                                            failure ,
                                             inputs ? [ ] ,
                                             mkDerivation ,
                                             name ,
